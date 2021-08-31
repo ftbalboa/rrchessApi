d499d4ed05eae4a1de10871d0a164d68842c0317
LAPTOP-MP5E825G\Francisco <ftbr0011@gmail.com>
2021-08-31 19:52:29 -0300

    heroku 6

--- index.js
+++ index.js
@@ -16,12 +16,13 @@
 //       }===={  }====={  }====={  }======{  }======{  }======={
 //      (______)(_______)(_______)(________)(________)(_________)
 
+
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // // Syncing all the models at once.
  conn.sync({ force: false }).then(() => {
-  server.listen(5000, () => {
-    console.log('%s listening at 5000'); // eslint-disable-line no-console
+  server.listen(process.env.PORT || 5000, () => {
+    console.log(`%s listening at ${process.env.PORT || 5000}`); // eslint-disable-line no-console
   });
  });
